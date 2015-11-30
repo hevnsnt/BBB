ed637402b3957d92bb4f3fd8fd2b5739206a6c51
Bill Swearingen <hevnsnt@gmail.com>
2015-11-29 19:08:30 -0600

    moved userlist to an include file

--- html/index.php
+++ html/index.php
@@ -34,10 +34,14 @@ $login = new Login();
 if ($login->isUserLoggedIn() == true) {
     // the user is logged in. you can do whatever you want here.
     // for demonstration purposes, we simply show the "you are logged in" view.
+    include("userlist.php");
+    include("macaddy.php");
     include("views/logged_in.php");
 
 } else {
     // the user is not logged in. you can do whatever you want here.
     // for demonstration purposes, we simply show the "you are not logged in" view.
+    include("userlist.php");
+    include("macaddy.php");
     include("views/not_logged_in.php");
 }
