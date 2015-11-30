f4d2ac6d321b9c761701275079daf719d94af9e7
Bill Swearingen <hevnsnt@gmail.com>
2015-11-29 19:37:27 -0600

    moved userlist to an include file

--- html/userlist.php
+++ html/userlist.php
@@ -6,18 +6,19 @@ $hostname = "localhost";
 //connection to the database
 $dbhandle = mysql_connect($hostname, $username, $password) 
  or die("Unable to connect to MySQL");
-echo "Connected to MySQL<br>";
+//echo "Connected to MySQL<br>";
 
 //select a database to work with
 $selected = mysql_select_db("seckc",$dbhandle) 
-  or die("Could not select seckc");
+//  or die("Could not select seckc");
 
 //execute the SQL query and return records
 $result = mysql_query("SELECT user_name, isBanned FROM users");
 
+echo "Player List"
 //fetch tha data from the database 
 while ($row = mysql_fetch_array($result)) {
-   echo "ID:".$row{'user_name'}." Name:".$row{'isBanned'}."<br>";
+   echo "User ID:".$row{'user_name'}." Banned:".$row{'isBanned'}."<br>";
 }
 //close the connection
 mysql_close($dbhandle);
