6ccf18c76c4430097b4ec0419bc87e04e872e153
Bill Swearingen <hevnsnt@gmail.com>
2015-11-29 19:17:59 -0600

    moved userlist to an include file

--- html/index.php
+++ html/index.php
@@ -27,7 +27,7 @@ require_once("config/db.php");
 require_once("classes/Login.php");
 
 // load userlist
-//include("userlist.php");
+
 
 // create a login object. when this object is created, it will do all login/logout stuff automatically
 // so this single line handles the entire login process. in consequence, you can simply ...
@@ -37,10 +37,12 @@ $login = new Login();
 if ($login->isUserLoggedIn() == true) {
     // the user is logged in. you can do whatever you want here.
     // for demonstration purposes, we simply show the "you are logged in" view.
+    include("userlist.php");
     include("views/logged_in.php");
 
 } else {
     // the user is not logged in. you can do whatever you want here.
     // for demonstration purposes, we simply show the "you are not logged in" view.
+    include("userlist.php");
     include("views/not_logged_in.php");
 }
