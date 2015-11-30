d934bb663c5d333b73feef93b0c749be53f07d24
Bill Swearingen <hevnsnt@gmail.com>
2015-11-29 20:01:39 -0600

    syntax issues

--- html/views/register.php
+++ html/views/register.php
@@ -25,20 +25,20 @@ $macaddress = returnmacAddress();
     <!-- the user name input field uses a HTML5 pattern check -->
     <label for="login_input_macaddress">Mac Address</label>
     <input id="login_input_macaddress" class="login_input" type="text" pattern="[a-zA-Z0-9]{2,64}" value="<?php echo $macaddress; ?>" name="macaddress" required /> 
-
+<br>
     <label for="login_input_username">Username (only letters and numbers, 2 to 64 characters)</label>
     <input id="login_input_username" class="login_input" type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" required /> 
-
+<br>
     <!-- the email input field uses a HTML5 email type check -->
     <label for="login_input_email">User's email</label>
     <input id="login_input_email" class="login_input" type="email" name="user_email" required />
-
-    <label for="login_input_password_new">Password (min. 6 characters)</label><br>
+<br>
+    <label for="login_input_password_new">Password (min. 6 characters)</label>
     <input id="login_input_password_new" class="login_input" type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" />
-
-    <label for="login_input_password_repeat">Repeat password</label><br>
+<br>
+    <label for="login_input_password_repeat">Repeat password</label>
     <input id="login_input_password_repeat" class="login_input" type="password" name="user_password_repeat" pattern=".{6,}" required autocomplete="off" />
-
+<br>
     <input type="submit"  name="register" value="Register" />
 </form>
 
