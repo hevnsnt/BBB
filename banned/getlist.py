c97a1c5ffeec40cec47d2139b24e6329a5621065
Bill Swearingen <hevnsnt@gmail.com>
2015-12-01 22:06:54 -0600

    Added more instructions

--- banned/getlist.py
+++ banned/getlist.py
@@ -32,7 +32,7 @@ def instructions(arg):
   length = int(len(str(os.path.abspath(arg))))
   print("#" * (length + 30))
   print("## Instructions for use:" + " " * (length + 4) + "##")
-  print("## edit /etc/hostapd/hostapd.conf to:" + " " * 11 + "##" )
+  print("## edit /etc/hostapd/hostapd.conf to:" + " " * (length + -4) + "##" )
   print("## deny_mac_file=%s") % os.path.abspath(arg)
   print("#" * 50)
   print
