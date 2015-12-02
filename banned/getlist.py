4372717108f3bd83f063e4093e90cd175596c411
Bill Swearingen <hevnsnt@gmail.com>
2015-12-01 22:00:17 -0600

    Added more instructions

--- banned/getlist.py
+++ banned/getlist.py
@@ -29,7 +29,7 @@ def readDb(write=False,file=""):
       display(row[0])
 
 def instructions(arg):
-  length = len(str(os.path.abspath(arg)))
+  length = int(len(str(os.path.abspath(arg))))
   print("#" * length + 10)
   print("## Instructions for use:" + " " * 24 + "##")
   print("## edit /etc/hostapd/hostapd.conf to:" + " " * 11 + "##" )
