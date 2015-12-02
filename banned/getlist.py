6edb87a2adbcff000cb56765497a434c6653a14d
Bill Swearingen <hevnsnt@gmail.com>
2015-12-01 21:42:28 -0600

    nope

--- banned/getlist.py
+++ banned/getlist.py
@@ -51,33 +51,5 @@ def main(argv):
   else:
     readDb()
 
-class PrintInColor:
-    RED = '\033[91m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    LIGHT_PURPLE = '\033[94m'
-    PURPLE = '\033[95m'
-    END = '\033[0m'
-
-    @classmethod
-    def red(cls, s, **kwargs):
-        print(cls.RED + s + cls.END, **kwargs)
-
-    @classmethod
-    def green(cls, s, **kwargs):
-        print(cls.GREEN + s + cls.END, **kwargs)
-
-    @classmethod
-    def yellow(cls, s, **kwargs):
-        print(cls.YELLOW + s + cls.END, **kwargs)
-
-    @classmethod
-    def lightPurple(cls, s, **kwargs):
-        print(cls.LIGHT_PURPLE + s + cls.END, **kwargs)
-
-    @classmethod
-    def purple(cls, s, **kwargs):
-        print(cls.PURPLE + s + cls.END, **kwargs)
-
 if __name__ == "__main__":
    main(sys.argv[1:])
